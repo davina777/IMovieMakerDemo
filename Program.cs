81163ed98d0cd701149625d993385eb7a4ed3580
davina777 <desu.avinash777@gmail.com>
2024-05-31 00:17:39 +0530

    Commiting Changes for MoviesController

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using IMovieMakerDemo;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddDbContext<MovieDbContext>(options =>
+    options.UseInMemoryDatabase("MoviesDb")
+);
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
